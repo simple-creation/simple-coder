78bb67ed3d6c3ec6e6d8560bdf722f47dc717ef2
windwithlife <zhangyq@trip.com>
2023-02-01 01:17:28 +0800

    upgrade v20230201

--- simple-coder.js
+++ simple-coder.js
@@ -94,6 +94,40 @@ const createProject = async (configData,workRootPath)=> {
     return workRootPath;
     
 }
+const createProjectExt = async (configData,workRootPath)=> {
+    let result = true;
+    if (!configData.name){
+        console.log('  *** application name not found!')
+        return "";
+    }
+    const appliationName = configData.name;
+    const templateName   = configData.templateName;
+
+    //const USER_HOME = process.env.HOME || process.env.USERPROFILE;
+    const tempPath = path.join(getTempWorkPath(),"/projects");
+
+    if(!workRootPath){
+        workRootPath = path.join(tempPath,appliationName);
+    }
+    if (fs.existsSync(workRootPath)) {
+        console.log(chalk.red('  ***WARNING!*** target directory is existed! '));
+        let removeFiles = 'rm -rf ' + workRootPath;
+        if(!executeTools.executeCommand(removeFiles,'remove old files')){
+            return false;
+        };
+    }
+    const loading = ora('processing ...');// 设置loading效果
+    //const template = await projectSetup.fetchTemplateConfig(templateName);
+    let template ={url:configData.url,id:configData.templateType}
+    const success = await projectSetup.initProjectByTemplateWithoutCompnents(template,configData,workRootPath);
+    if (!success){
+        loading.fail(chalk.red('project creating failure !'));
+        return "";
+    }
+    loading.succeed('project creating success!');
+    return workRootPath;
+    
+}
 const getTempWorkPath = (newPath) =>{
    return projectSetup.getTempWorkPath(newPath);
 }
@@ -103,4 +137,5 @@ const getSupportApplications = () =>{
 module.exports.getSupportApplications = getSupportApplications;
 module.exports.getTempWorkPath = getTempWorkPath;
 module.exports.createProject = createProject;
+module.exports.createProjectExt = createProjectExt;
 module.exports.createProjectSet = createProjectSet;
